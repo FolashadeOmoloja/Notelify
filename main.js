93f98a8a32c1974c01acd013ce8b163dbb3c59ee
FolashadeOmoloja <omolojashade@gmail.com>
2022-10-18 12:00:44 +0100

    changes made

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
 let btn2 = document.querySelector("#bt-2");
 let dropUp = document.querySelector(".drop-up")
-btn2.addEventListener('click',displaybtn2);
-
-function displaybtn2(){
+let setIcon = document.querySelector(".set-icon")
+let setting = document.querySelector('.setting')
+let setIconNote = document.querySelector(".set-icon-note")
+let settingNote = document.querySelector('.setting-note')
+let getCancelButton = document.querySelector("#cancel")
+btn2.addEventListener('click',() => {
     dropUp.style.display = 'block'
-}
+});
+setIcon.addEventListener('click',() => {
+    setting.style.display = 'block'
+});
+setIconNote.addEventListener('click',() => {
+    settingNote.style.display = 'block'
+});
+getCancelButton.addEventListener('click', () => {
+    document.querySelector('.pop-up').style.display = 'none'
+})
+
 
 window.onclick = function (event) {
     if (!event.target.matches('#bt-2')) {
         
             dropUp.style.display = "none";
     }
+    
+    if (!event.target.matches('.set-icon')) {
+        
+        setting.style.display = "none";
+}
+if (!event.target.matches('.set-icon-note')) {
+        
+    settingNote.style.display = "none";
+}
 }   
 
-/* .sam{
-    background-color: white;
-    padding: 20px;
-    color: black;
-    display: flex;
-}
-
-.sam input{
-    background-color: white;
-    outline: none;
-    font-size: 16px;
-    height: 17px;
-    width: 500px;
-    border: none;
-} */
